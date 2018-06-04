68e39b63e6ffe13ff6b0b02edc158ff9d0af8f09
TsvetanMilanov <tsvetan.milanov17@gmail.com>
2018-06-04 21:15:55 +0300

    Add match replacer examples

--- filter/example_configuration_test.go
+++ filter/example_configuration_test.go
@@ -100,3 +100,43 @@ func ExamplePersonalDataFilterBuilder_WithAdditionalRegularExpressions() {
 	// Output:
 	// struct { Personal string; MyProp string }{Personal:"", MyProp:"not"}
 }
+
+func ExamplePersonalDataFilterBuilder_WithDefaultMatchReplacer() {
+	f, err := filter.NewBuilder().
+		WithDefaultMatchReplacer().
+		Build()
+	if err != nil {
+		panic(err)
+	}
+
+	input := struct {
+		Personal string
+		MyProp   string
+	}{
+		Personal: "email@mail.com", // will be replaced and the result will be sha256 hash
+		MyProp:   "not-personal",   // will not be replaced
+	}
+	fmt.Printf("%#v\n", f.RemovePersonalData(input))
+	// Output:
+	// struct { Personal string; MyProp string }{Personal:"3d13579f08e876d2d2d94da15ea657fb39795dd2a59e3378c9e58c4f4b0d053b", MyProp:"not-personal"}
+}
+
+func ExamplePersonalDataFilterBuilder_WithMatchReplacer() {
+	f, err := filter.NewBuilder().
+		WithMatchReplacer(func(input string) string { return input + "-replaced" }).
+		Build()
+	if err != nil {
+		panic(err)
+	}
+
+	input := struct {
+		Personal string
+		MyProp   string
+	}{
+		Personal: "email@mail.com", // will be replaced and the result will be email@mail.com-replaced
+		MyProp:   "not-personal",   // will not be replaced
+	}
+	fmt.Printf("%#v\n", f.RemovePersonalData(input))
+	// Output:
+	// struct { Personal string; MyProp string }{Personal:"email@mail.com-replaced", MyProp:"not-personal"}
+}
